e90ab61b67d17e9c25b1541005d778fd799e168e
unknown <3933714051@bitraipur.ac.in>
2022-02-22 12:10:37 +0530

    first commit: create the directory structure and setup the express server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port,function(err){
+    if(err){
+        // here new menthod is used to write err in between string the key before 1 is used here
+        // this method is called interpolation
+        // this evaluetes the word inside the bracket after doller like ${2+2} will be 3
+        console.log(`Error in running the server:,${err}`);
+    }
+    console.log(`server is running on port: ${port}`);
+});
