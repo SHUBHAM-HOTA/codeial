460cd62af5ae126bc3a4f41552a2055de715879c
unknown <3933714051@bitraipur.ac.in>
2022-03-01 20:40:44 +0530

    added the comment deleting feature

--- routes/comments.js
+++ routes/comments.js
@@ -9,4 +9,6 @@ const commentsController = require('../controllers/comments_controller');
 // here we are checking the authentication so that the only persion that is signed in can post not others
 router.post('/create',passport.checkAuthentication, commentsController.create);
 
+router.get('/destroy/:id',passport.checkAuthentication, commentsController.destroy)
+
 module.exports = router;
