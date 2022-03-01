9df45520fccaca64fd751b2eb36b25664416fd09
unknown <3933714051@bitraipur.ac.in>
2022-03-01 19:42:04 +0530

    added the delete post feature

--- routes/posts.js
+++ routes/posts.js
@@ -9,4 +9,6 @@ const postsController = require('../controllers/posts_controller');
 // here we are checking the authentication so that the only persion that is signed in can post not others
 router.post('/create',passport.checkAuthentication,postsController.create);
 
+router.get('/destroy/:id', passport.checkAuthentication, postsController.destroy);
+
 module.exports = router;
